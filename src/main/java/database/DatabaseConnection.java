3346d8c20d01165927d6cad84f981ccd07ef4292
S'dhari Hardy <sdharihardy8@yahoo.com>
2025-11-24 18:45:46 -0500

    Created final version to submit.

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -5,10 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-// Connects DB to logic
 public class DatabaseConnection {
 
-    private static final String dbPath= "src/main/java/database/database.db";
+    private static final String dbPath = "database.db"; // <- Put the db file here
 
     public static Connection getConnection() throws SQLException {
         try {
@@ -17,8 +16,9 @@ public class DatabaseConnection {
         } catch (ClassNotFoundException e) {
             throw new SQLException("SQLite JDBC driver not found. Make sure the sqlite-jdbc jar is on the classpath.", e);
         }
-        System.out.println("Trying to connect to database...");
-        return DriverManager.getConnection("jdbc:sqlite:"+dbPath);
-    }
 
+        System.out.println("Trying to connect to database at: " + new File(dbPath).getAbsolutePath());
+        return DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+    }
 }
+
